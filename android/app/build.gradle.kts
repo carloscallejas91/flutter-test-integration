d009abca09fb4ba731c5a4f53941ac27ed723dab
carloscallejas <carloscallejas91@outlook.com>
2025-09-05 17:13:07 -0300

    Remove Portuguese comment from GitHub Actions workflow
    This commit removes a Portuguese comment from the top of the `main.yml` GitHub Actions workflow file. The comment, `# Nome do workflow que aparecerá no separador "Actions" do GitHub.`, described that the `name` field defines the workflow name displayed in the GitHub Actions tab. This change has no functional impact on the workflow.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,53 +1,55 @@
 plugins {
     id("com.android.application")
-    id("com.google.gms.google-services")
-    id("kotlin-android")
+    kotlin("android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-configurations.all {
-    resolutionStrategy.force("androidx.test:runner:1.5.2")
-    resolutionStrategy.force("androidx.test:rules:1.5.0")
-    resolutionStrategy.force("androidx.test.espresso:espresso-core:3.5.1")
-    resolutionStrategy.force("androidx.test.espresso:espresso-idling-resource:3.5.1")
+fun localProperties(): java.util.Properties {
+    val properties = java.util.Properties()
+    val localPropertiesFile = rootProject.file("local.properties")
+    if (localPropertiesFile.exists()) {
+        properties.load(java.io.FileInputStream(localPropertiesFile))
+    }
+    return properties
 }
+
+val flutterVersionCode: String by project
+val flutterVersionName: String by project
+
 android {
     namespace = "com.test_integration.test_integration_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 34
+    ndkVersion = "25.1.8937393"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "1.8"
+    }
+
+    sourceSets {
+        getByName("main") {
+            java.srcDirs("src/main/kotlin")
+        }
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.test_integration.test_integration_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    testOptions {
-        execution = "ANDROIDX_TEST_ORCHESTRATOR"
-        animationsDisabled = true
-
-        unitTests.isReturnDefaultValues = true
+        minSdk = 21
+        targetSdk = 34
+        versionCode = flutterVersionCode.toInt()
+        versionName = flutterVersionName
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
@@ -55,3 +57,11 @@ android {
 flutter {
     source = "../.."
 }
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
