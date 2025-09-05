6fd13c672889a7fb293f967577e6db5903c0069b
carloscallejas <carloscallejas91@outlook.com>
2025-09-05 18:03:01 -0300

    test integration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,16 +1,13 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
     id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-configurations.all {
-    resolutionStrategy.force("androidx.test:runner:1.5.2")
-    resolutionStrategy.force("androidx.test:rules:1.5.0")
-    resolutionStrategy.force("androidx.test.espresso:espresso-core:3.5.1")
-    resolutionStrategy.force("androidx.test.espresso:espresso-idling-resource:3.5.1")
-}
 android {
     namespace = "com.test_integration.test_integration_app"
     compileSdk = flutter.compileSdkVersion
@@ -25,12 +22,6 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
-    sourceSets {
-        getByName("main") {
-            java.srcDirs("src/main/kotlin")
-        }
-    }
-
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.test_integration.test_integration_app"
@@ -40,14 +31,6 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        testInstrumentationRunnerArguments["clearPackageData"] = "true"
-    }
-
-    testOptions {
-        execution = "ANDROIDX_TEST_ORCHESTRATOR"
-        animationsDisabled = true
-
-        unitTests.isReturnDefaultValues = true
     }
 
     buildTypes {
@@ -61,11 +44,4 @@ android {
 
 flutter {
     source = "../.."
-}
-
-dependencies {
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test:rules:1.5.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
