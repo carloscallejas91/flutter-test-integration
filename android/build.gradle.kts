d4121acaf5024479077fa0b780fc6ec526b834e8
carloscallejas <carloscallejas91@outlook.com>
2025-09-08 13:58:52 -0300

    test integration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,7 @@
 plugins {
-    id("com.android.application") version "8.2.2" apply false
+    // A versão foi removida para resolver o conflito.
+    id("com.android.application") apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
-
-    // ADICIONADO: Plugin do Google Services para o projeto, necessário para o Firebase.
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
@@ -24,7 +22,6 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-
 subprojects {
     project.evaluationDependsOn(":app")
 }
