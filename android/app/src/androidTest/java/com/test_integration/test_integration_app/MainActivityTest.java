597d1ae1db3dc35a6e0f8042ba3b0167c64bf3d5
carloscallejas <carloscallejas91@outlook.com>
2025-09-10 08:39:50 -0300

    test integration

---
+++ android/app/src/androidTest/java/com/test_integration/test_integration_app/MainActivityTest.java
@@ -0,0 +1,10 @@
+package com.test_integration.test_integration_app
+
+import androidx.test.rule.ActivityTestRule;
+import dev.flutter.plugins.integration_test.FlutterTestRunner;
+import org.junit.Rule;
+import org.junit.runner.RunWith;@RunWith(FlutterTestRunner.class)
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, false);
+}
