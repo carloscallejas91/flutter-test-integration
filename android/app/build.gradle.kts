a3679b9db127aa1a06ecd1e93b25f96cc9af443c
carloscallejas <carloscallejas91@outlook.com>
2025-09-10 17:23:53 -0300

    test integration 2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     // Dependências de Teste (versões modernas e compatíveis com o Orquestrador)
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit-ktx:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
 
     // Dependências para o Orquestrador de Testes
     androidTestUtil("androidx.test:orchestrator:1.5.0")
