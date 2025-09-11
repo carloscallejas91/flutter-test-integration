6d1bae3cdd05e6fcc6a70e7c26c26b518cc1309a
carloscallejas <carloscallejas91@outlook.com>
2025-09-10 22:17:15 -0300

    test integration 2

--- android/app/src/androidTest/java/com/test_integration/test_integration_app/MainActivityTest.kt
+++ android/app/src/androidTest/java/com/test_integration/test_integration_app/MainActivityTest.kt
@@ -1,14 +1,7 @@
 package com.test_integration.test_integration_app
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import dev.flutter.plugins.integration_test.FlutterTestRunner
-import org.junit.Rule
 import org.junit.runner.RunWith
 
 @RunWith(FlutterTestRunner::class)
-class MainActivityTest {
-    @Rule
-    @JvmField
-    val rule = ActivityScenarioRule(MainActivity::class.java)
-}
-
+class MainActivityTest
